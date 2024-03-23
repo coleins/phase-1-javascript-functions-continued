1bae10791a212c2406fe3b58f3227a6382d9ccf8
Collins Ngetich <collinsngetichk@gmail.com>
2024-03-23 23:42:50 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun());
+
+function mondayWork(whatsUp = "go to the office") {
+  return `This Monday, I will ${whatsUp}.`;
+}
+console.log(mondayWork());
+
+function wrapAdjective(symbol = "*") {
+  return function (adjective) {
+    return `You are ${symbol}${adjective}${symbol}!`;
+  };
+}
